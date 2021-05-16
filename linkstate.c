8228d2d297bbb049cad54d52ee83e7269f37703c
heewonham <huiwonham6@u.sogang.ac.kr>
2021-05-16 20:31:40 +0900

    Computer network HW2 - Routing Protocols -- (linkstate)

--- linkstate.c
+++ linkstate.c
@@ -5,27 +5,26 @@
 #define MAX_STR 1000
 #define MAX_NODE 100
 
-void Dijkstra(int node, int src, int(*adj_graph)[100]);
-void find_p(int src, int dest, int dst, FILE *fp);
-int table[100][100][2];
+int topology(FILE *fwp, char* topofile, int *node);
+int message(FILE *fwp, char* mesgfile);
+int change(FILE* fwp, char* chgefile, char* mesgfile, int node);
+void Dijkstra(int node, int src);
+int write_route(int src, int dest, int dst, FILE* fwp);
+int find_parent(int src, int dest, int prev);
+
+int table[MAX_NODE][MAX_NODE][2];
+int adj_graph[MAX_NODE][MAX_NODE];
 
 int main(){//int argc, char *argv[]){
 		
 	char topofile[MAX_STR] = { '\0', };
 	char mesgfile[MAX_STR] = { '\0', };
 	char chgefile[MAX_STR] = { '\0', };
-	char mesg[MAX_STR] = { '\0', };
-	char buf[MAX_STR] = { '\0', };
+	char buf[MAX_STR] = { '\0', }; // 삭 제 
 	char *tmp1;
 	
-	int node; 	
-	int src; 		// 출발 정점
-	int n1, n2, cost;
-	int adj_graph[MAX_NODE][MAX_NODE]; // 최대 100개 이하의 vertex만 들어옴
-	int i, j;
-	
-	FILE * fwp;
-	FILE * fp;
+	int node; 		
+	FILE * fwp = fopen("output_ls.txt","w");
 	
 	// 1. 인자수  
 	scanf("%[^\n]", buf);
@@ -43,90 +42,28 @@ int main(){//int argc, char *argv[]){
 	strcpy(chgefile, tmp1);
 
 	// 3-1. topology open 
-	fp = fopen(topofile, "r");
-	fwp = fopen("output_ls.txt","w");
-	if (fp == NULL) {
-		printf("Error: open input file.\n");
-		return -1;
-	}
-	
-	fgets(buf, MAX_STR, fp);
-	sscanf(buf, "%d", &node);
-	for(i = 0; i < node; i++)
-		for(j = 0; j < node; j++){
-			adj_graph[i][j] = 999;
-			table[i][j][0] = 999;
-			table[i][j][1] = 999;
-		}
-			
-			
-	while(fgets(buf, MAX_STR, fp)){
-		sscanf(buf, "%d %d %d", &n1, &n2, &cost);
-		adj_graph[n1][n2] = cost;
-		adj_graph[n2][n1] = cost;
-	}
-	
-	for(i = 0; i < node; i++ ){
-		// 초기화
-		table[i][i][1] = 0;
-		Dijkstra(node, i, adj_graph);
-	}
-	for(i =0; i < node; i++){
-		for(j = 0; j < node; j++){
-			printf("%d %d %d\n",j, table[j][i][0], table[j][i][1]);
-		}
-		printf("\n");
-	}
-	fclose(fp);
+	if(topology(fwp, topofile, &node) == -1)
+		exit(1);
 
 	// 3-2. message
-	fp = fopen(mesgfile, "r");
-	if (fp == NULL) {
-		printf("Error: open input file.\n");
-		return -1;
-	}
-		
-	while(fgets(buf, MAX_STR, fp)){
-		sscanf(buf, "%d %d %s", &n1, &n2, mesg);
-		strcpy(mesg,buf+4);
-		if(table[n1][n2][1] == 999){
-			printf("from %d to %d cost infinite hops unreachable message %s\n",n1,n2,mesg);
-		}
-		else{
-			printf("from %d to %d cost %d hops ",n1,n2,table[n1][n2][1]);
-			find_p(n1,n2,n2,fp);
-			printf("message %s\n",mesg);
-		}
-	}
-	fclose(fp);
-	
+	if(message(fwp, mesgfile) == -1)
+		exit(1);
 	
 	// 3-3. changes
+	if(change(fwp, chgefile, mesgfile, node) == -1)
+		exit(1);
 	
 	// 4. finish
-	printf("Complete. Output file written to ouput_ls.txt.\n"); 
+	printf("Complete. Output file written to ouput_ls.txt."); 
 	
 }
-void find_p(int src, int dest, int dst, FILE *fp) {
-	if (dest == src) {
-		printf("%d ", dest);
-		return;
-	}
-	else {
-		find_p(src, table[src][dest][0], dst ,fp); //도착점이 출발점이 아닌경우 경유하는 점을 탐색
-		if(dest != dst)
-			printf("%d ", dest);
-		return;
-	}
-}
-void Dijkstra(int node, int src, int(*adj_graph)[100]) {
+void Dijkstra(int node, int src) {
 
 	int min; 		// 최단 경로가 결정된 vertex의 dist 값 
 	int index;		// 최단 경로가 결정된 vertex 중 가장 작은 값의 index
 	int i, j, k;
 	int found[MAX_NODE]; // vertex 방문 체크 표시, 0은 false, 1은 true (1은 최단경로가 결정되고 방문한다는 의미이다)   
-	// neighbor 배열을 이용하는 Dijkstra 알고리즘을 작성합니다.
-	// 최단 경로가 결정되지 않은 vertex의 found를 0(false)으로 초기화 
+
 	for (i = 0; i < node; i++) {
 		found[i] = 0; 
 	}
@@ -137,7 +74,8 @@ void Dijkstra(int node, int src, int(*adj_graph)[100]) {
 
 	for (i = 0; i < node - 1; i++) { //총 n-1번을 반복한다.
 		for (j = 0; j < node; j++) { // 최소거리의 index와 연결된 점의 최소거리를 갱신 
-			if ((table[src][index][1] + adj_graph[index][j]) < table[src][j][1]) { // 기존의 거리보다 index를 경유한 거리가 더 짧을 경우, 경유하는 거리로 dist의 값을 갱신한다.
+			if ((table[src][index][1] + adj_graph[index][j]) < table[src][j][1] 
+			|| ((table[src][index][1] + adj_graph[index][j]) == table[src][j][1] && table[src][j][0] > index)) { // 기존의 거리보다 index를 경유한 거리가 더 짧을 경우, 경유하는 거리로 dist의 값을 갱신한다.
 				table[src][j][1] = table[src][index][1] + adj_graph[index][j];
 				table[src][j][0] = index;
 			}
@@ -153,4 +91,145 @@ void Dijkstra(int node, int src, int(*adj_graph)[100]) {
 		}
 		found[index] = 1; // index를 방문한다는 표시. 
 	}
+}
+int write_route(int src, int dest, int dst, FILE* fwp) {
+	if (dest == src) {
+		fprintf(fwp,"%d ", dest);
+		return;
+	}
+	else {
+		write_route(src, table[src][dest][0], dst ,fwp); //도착점이 출발점이 아닌경우 경유하는 점을 탐색
+		if(dest != dst)
+			fprintf(fwp,"%d ", dest);
+		return;
+	}				 
+}
+int find_parent(int src, int dest, int prev) {
+	if (dest == src) {
+		return prev;
+	}
+	else {
+		return find_parent(src, table[src][dest][0], dest); //도착점이 출발점이 아닌경우 경유하는 점을 탐색
+	}				 
+}
+int topology(FILE *fwp, char* topofile, int *node) {
+	
+	char buf[MAX_STR] = { '\0', };
+	FILE * fp = fopen(topofile, "r");
+	int n1, n2, cost;
+	int i, j;
+
+	if (fp == NULL) {
+		printf("Error: open input file.\n");
+		return -1;
+	}
+	
+	fgets(buf, MAX_STR, fp);
+	sscanf(buf, "%d", &(*node));
+	
+	// 초기화 
+	for(i = 0; i < *node; i++)
+		for(j = 0; j < *node; j++){
+			adj_graph[i][j] = 999;
+			table[i][j][0] = 999;
+			table[i][j][1] = 999;
+		}
+			
+			
+	while(fgets(buf, MAX_STR, fp)){
+		sscanf(buf, "%d %d %d", &n1, &n2, &cost);
+		if(cost == -999) cost = 999;
+		adj_graph[n1][n2] = cost;
+		adj_graph[n2][n1] = cost;
+	}
+	
+	for(i = 0; i < *node; i++ ){
+		// 초기화
+		table[i][i][1] = 0;
+		Dijkstra(*node, i);
+	}
+	for(i =0; i < *node; i++){
+		for(j = 0; j < *node; j++){
+			if(table[i][j][1] != 999)
+				fprintf(fwp,"%d %d %d\n",j, find_parent(i,j,i),table[i][j][1]);
+		}
+		fprintf(fwp,"\n");
+	}
+	fclose(fp);
+	return 0;
+}
+
+int message(FILE *fwp, char* mesgfile) {
+	
+	char buf[MAX_STR] = { '\0', };
+	char mesg[MAX_STR] = { '\0', };
+	FILE * fp = fopen(mesgfile, "r");
+	int n1, n2;
+	
+	if (fp == NULL) {
+		printf("Error: open input file.\n");
+		return -1;
+	}
+		
+	while(fgets(buf, MAX_STR, fp)){
+		sscanf(buf, "%d %d %s", &n1, &n2, mesg);
+		strcpy(mesg,buf+4);
+		if(table[n1][n2][1] == 999){
+			fprintf(fwp,"from %d to %d cost infinite hops unreachable message %s\n",n1,n2,mesg);
+		}
+		else{
+			fprintf(fwp,"from %d to %d cost %d hops ",n1,n2,table[n1][n2][1]);
+			write_route(n1, n2, n2, fwp);
+			fprintf(fwp,"message %s",mesg);
+		}
+	}
+	fprintf(fwp,"\n");
+	fclose(fp);
+	return 0;
+}
+
+int change(FILE* fwp, char* chgefile, char* mesgfile, int node) {
+	
+	char buf[MAX_STR] = { '\0', };
+	FILE * fp = fopen(chgefile, "r");
+	int n1, n2, cost;
+	int i, j;
+
+	if (fp == NULL) {
+		printf("Error: open input file.\n");
+		return -1;
+	}
+		
+	while(fgets(buf, MAX_STR, fp)){
+		sscanf(buf, "%d %d %d", &n1, &n2, &cost);
+		if(cost == -999) cost = 999;
+		if(adj_graph[n1][n2] < cost){
+			for(i = 0; i < node; i++){
+				for(j = 0; j < node; j++){
+					table[i][j][0] = 999;
+					table[i][j][1] = 999;
+				}
+			}
+		}
+
+		adj_graph[n1][n2] = cost;
+		adj_graph[n2][n1] = cost;
+		
+		for(i = 0; i < node; i++ ){
+			table[i][i][1] = 0;
+			Dijkstra(node, i);
+		}
+		for(i =0; i < node; i++){
+			for(j = 0; j < node; j++){
+				if(table[i][j][1] != 999)
+					fprintf(fwp,"%d %d %d\n",j, find_parent(i,j,i),table[i][j][1]);
+			}
+			fprintf(fwp,"\n");
+		}
+		
+		message(fwp,mesgfile);
+	}
+	
+	fclose(fp);
+	return 0;
 }
