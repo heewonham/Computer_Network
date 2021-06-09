6c9dfe27a509f5f4708214e400e23edb1e06c564
heewonham <huiwonham6@u.sogang.ac.kr>
2021-06-09 14:52:23 +0900

    Computer network HW3 - Cyclic Redundancy Check : crc_encode

--- crc_encoder.cpp
+++ crc_encoder.cpp
@@ -1,19 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 # define MAX 1000
 
 using namespace std;
 string binary;
-vector<int> result;
+vector<char> result;
 
-void make_binary(char buf);
+void make_binary(int num);
 void crc_encode(string dataword, string generator, int datasize);
+void write_output(FILE* fwp);
 
 int main() {//int argc, char *argv[]){
 
-	int i, j, padsize;
-	int datasize, len = 0;
+	int i, j, datasize;
 	string generator, dataword;
 	char buf[MAX] = {'\0',} , input[MAX] = { '\0', }, output[MAX] = { '\0', };
 	FILE* fwp, * fp;
@@ -44,14 +46,13 @@ int main() {//int argc, char *argv[]){
 	}
 
 	while (fread(buf, sizeof(char), MAX, fp)) {
-		len += strlen(buf);
-		for(i=0; i< len; i++){
+		for(i=0; i< (int)strlen(buf); i++){
 			// 1. 이진수 변환
-			make_binary(buf[i]);
+			make_binary((int)buf[i]);
 			// 2. crc_decode
 			for (j = 0; j < 8; j++) {
 				dataword += binary.at(j);
-				if (dataword.size() == datasize) {
+				if ((int)dataword.size() == datasize) {
 					crc_encode(dataword, generator, datasize);
 					dataword = "";
 				}
@@ -59,14 +60,15 @@ int main() {//int argc, char *argv[]){
 		}
 	}
 
-	/*padsize = 8 - (result.size() % 8);
-	while (1) {
-
-	}*/
+	write_output(fwp);
+	fclose(fp);
+	fclose(fwp);
 }
-void make_binary(char buf) {
-	int i, num = (int)buf;
+void make_binary(int num) {
+
+	int i;
 	char tmp;
+	binary = "";
 
 	for (i = 7; i >= 0; --i) {
 		tmp = (num >> i & 1)+'0';
@@ -75,29 +77,56 @@ void make_binary(char buf) {
 }
 void crc_encode(string dataword, string generator, int datasize) {
 	int i, j, num;
-	int gen = stoi(generator);
-	string reminder, operand, data = dataword;
-	string tmp;
+	string tmp, operand, data = dataword;
 
-	for (i = 0; i < generator.size()-1; i++)
+	for (i = 0; i < (int)generator.size()-1; i++)
 		data += "0";
 	operand = data.substr(0, datasize);
 
-	//for (i = datasize; i < data.size(); i++) {
-	i = datasize;
-	while(i <= data.size()){
-		if (operand.at(0) == '0') {
+	for (i = datasize; i <= (int)data.size(); i++) {
+		if (operand.at(0) == '0')
 			operand.erase(0, 1);
-			operand += data.at(i++);
-		}
 		else {
 			tmp = "";
-			for (j = 0; j < operand.size(); j++) {
-				num = operand.at(j) - '0' ^ generator.at(j) - '0';
+			for (j = 1; j < (int)operand.size(); j++) {
+				num = (operand.at(j) - '0') ^ (generator.at(j) - '0');
 				tmp += num + '0';
 			}
 			operand = tmp;
-			cout << operand << endl;
 		}
-	} 
+		if (i < (int)data.size())
+			operand += data.at(i);
+	}
+
+	for (i = 0; i < (int)dataword.size(); i++)
+		result.push_back(dataword[i]);
+	for (i = 0; i < (int)operand.size(); i++)
+		result.push_back(operand[i]);
+}
+void write_output(FILE* fwp) {
+	
+	int i, padsize;
+	char buf;
+	string codeword = "";
+
+	// 1. 패딩사이즈 
+	padsize = 8 - (result.size() % 8);
+	make_binary(padsize);
+	buf = (char)stoi(binary);
+	fwrite(&buf, sizeof(char), 1, fwp);
+
+	// 2. 패딩포함
+	for (i = 0; i < padsize; i++) {
+		codeword += "0";
+	}
+
+	// 3. codeword 
+	for (i = 0; i < (int)result.size(); i++) {
+		codeword += result[i];
+		if ((i + padsize+1) % 8 == 0) {
+			buf = (char)stoi(codeword);
+			fwrite(&buf, sizeof(char), 1, fwp);
+			codeword = "";
+		}
+	}
 }
